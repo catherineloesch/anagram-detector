f95e5084add2ad75c7bd0ba9dbbeec2c82df16aa
Catherine Loesch <catherine.loesch@protonmail.com>
2021-02-14 00:48:59 +0000

    learn test successful

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word, :word_array, :possible_anagrams, :possible_anagrams_array, :matching_word
+
+    def initialize(word)
+        @word = word
+        @word_array = @word.split("").sort
+        @word_array
+    end
+
+    def match(possible_anagrams)
+        
+        @possible_anagrams_array = []
+        @matching_word = []
+        @possible_anagrams = possible_anagrams
+        @possible_anagrams.map {|word| @possible_anagrams_array << word.split("")}
+
+        @possible_anagrams_array.each do |item|
+            if item.sort == @word_array
+                @matching_word << item.join
+            end
+        end
+        @matching_word
+    end
+end
+
